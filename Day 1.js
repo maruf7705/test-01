8fecefbbc2ec52f60dd23872756c9ff69a97f857
Maruf Islam <113976731+maruf7705@users.noreply.github.com>
2025-05-18 10:28:07 +0600

    1day 1hour

---
+++ Day 1.js
@@ -0,0 +1,35 @@
+console.log("Maruf again revisiton of javascript")
+console.log("I Love Ai and Robotics")
+console.log("I love coludy weather")
+console.log("I love to learn new skills")
+
+// Step 2 
+fullname = "Sadik"
+console.log(fullname)
+Pirce = 99.5
+console.log(Pirce)
+
+// Step 3
+a = null
+b = undefined
+console.log(a)
+console.log(b)
+console.log(a + b)
+// Step 4
+
+isfollow = true
+isfalse = false
+console.log(isfalse)
+console.log(isfollow)
+
+// Step 5
+
+Maruf$love = "sweets"
+Maruf$learing = "javascript"
+console.log(Maruf$love)
+console.log(Maruf$learing)
+Maruf1 = "Maruf Learing "
+Maruf_learing = true
+console.log(Maruf1)
+console.log(Maruf_learing)
+// Step 6
