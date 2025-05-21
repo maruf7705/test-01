4641bd5481e4a6dfb5bde6a9c8800f6514580bbe
Maruf Islam <113976731+maruf7705@users.noreply.github.com>
2025-05-21 23:19:50 +0600

    done

--- DAY 3/La 2/script.js
+++ DAY 3/La 2/script.js
@@ -1,12 +1,29 @@
 //Artimetic Operators
 
-let a = 21;
-let b = 5;
+// let a = 21;
+// let b = 5;
 
-console.log("a=",a,"&","b=",b)
-console.log("a+b= ",a+b);
-console.log("a-b= ",a-b);
-console.log("a*b= ",a*b);
-console.log("a/b= ",a/b);
-console.log("a%b= ",a%b);
-console.log("a**b=",a**b)
+// console.log("a=",a,"&","b=",b)
+// console.log("a+b= ",a+b);
+// console.log("a-b= ",a-b);
+// console.log("a*b= ",a*b);
+// console.log("a/b= ",a/b);
+// console.log("a%b= ",a%b);
+// console.log("a**b=",a**b)
+
+// a--
+// console.log("a =",a)
+
+// unary Oparetor
+
+let a =5; 
+let b =2;
+
+console.log("a=",a,"&","b",b)
+console.log("a+b",a+b)
+
+a++;
+console.log("a=",a);
+--a;
+console.log("a=",a)
+
