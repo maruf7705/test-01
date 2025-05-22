424349bc65ed9c8516d79194c7157ca88d912271
Maruf Islam <113976731+maruf7705@users.noreply.github.com>
2025-05-22 15:09:49 +0600

    .

---
+++ DAY 5/Number-find.js
@@ -0,0 +1,16 @@
+number = 5
+// যদি ২ ‍দিয়ে ভাগ দিয়ে উত্তর ০ হয় তাহলে ওটাই যোর আর যদি আন্য কিছু আসে তাহলে বিজোড়
+if (number%2 === 0)
+{
+    console.log("the number is Even")
+}
+else{
+    console.log("The number is Odd")
+}
+
+
+
+// let a = 5;
+// let b = "2";
+
+// console.log("a === b", a === b)
--- DAY 5/script2.js
+++ DAY 5/script2.js
@@ -1,10 +1,24 @@
  
- age = 17;
+//  age = 17;
 
- if (age>18){
-    console.log("You can vote")
- }
+//  if (age>18){
+//     console.log("You can vote")
+//  }
  
- if (age<18){
-    console.log("You can't vote")
- }
+//  if (age<18){
+//     console.log("You can't vote")
+//  }
+
+let mode = "dark"
+let color;
+
+if (mode === "dark"){
+    color = "black"
+}
+else{
+    color = "white"
+}
+
+console.log(color);
+
+// Today I also learn If and else but this also like python.
