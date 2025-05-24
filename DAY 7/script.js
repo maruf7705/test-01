07b13012a555874f1daad2dcbd256fc704b40c81
Maruf Islam <113976731+maruf7705@users.noreply.github.com>
2025-05-24 21:00:22 +0600

    update
    Loop

---
+++ DAY 7/script.js
@@ -0,0 +1,23 @@
+// for (let i=1; i<=10000;i++) {
+//     console.log("You are my love Megla");
+// }
+
+// let sum =0;
+// let n = 100;
+// for (let plus=1; plus<=n;plus++){
+//     sum = sum+ plus
+// }
+
+// console.log("Sum =",sum)
+
+// for (i =1; i<=5;i++){
+//     console.log("You are my love Megla",i);
+// }
+
+
+let love = 1;
+
+while(love<=500){
+ console.log("Megla I love you",love);
+ love++;
+}
