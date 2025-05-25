4aacdb8eeddf6d31e59b35ce95df8d24ebe3f0a5
Maruf Islam <113976731+maruf7705@users.noreply.github.com>
2025-05-25 13:11:43 +0600

    update

---
+++ DAY 8/script.js
@@ -0,0 +1,29 @@
+// for-of loop
+
+// let str = "Megla Love";
+
+// for(let i of str){
+//     console.log("i=",i)
+// }
+
+// for (i=1;i<=5;i++){
+    
+// }
+// console.log(i)
+
+// let sum = 0;
+// let n = 10;
+
+// for (i=0;i<=n;i++){
+//     sum = sum+i
+// }
+
+// console.log(sum)
+
+let fullname = "codenestx";
+let fullname_size = 0;
+for (let i of fullname){
+    console.log(i);
+fullname_size++;
+}
+console.log(fullname_size)
