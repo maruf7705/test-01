ffa76dbe0722d538085a40ccc6e077b54eec61cd
Maruf Islam <113976731+maruf7705@users.noreply.github.com>
2025-05-26 20:35:19 +0600

    done

---
+++ DAY 9/script.js
@@ -0,0 +1,39 @@
+ 
+//  gmaenum = 25;
+
+
+//  usernum = prompt("what is your number");
+
+//  while(usernum != gmaenum)
+// {
+// usernum = prompt("try again , this is not your correct")
+// }
+
+
+// alert("Congratulation You find your number")
+
+
+// let full = "Maruf Islam"
+
+// console.log(full[0])
+
+// let sentence = ``
+
+// let obj = {
+// name:"Pen",
+// price:"10"
+// }
+
+// console.log("This is",obj.name,"and price is",obj.price,"taka")
+
+// let obj = {
+// name:"Pen",
+// price:"10"
+// }
+
+// let output = `The cost of ${obj.name} and price is ${obj.price} Taka`
+// console.log(output)
+
+
+let str = ("Maruf\nIslam") 
+console.log(str.toUpperCase())
